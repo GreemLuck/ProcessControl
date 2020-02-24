c088019900433bca60866327cc08a93d42b29b23
Gremaud Lucien <lucien.gremaud@gmail.com>
2020-02-24 16:44:44 +0100

    setup display class

--- carPID/src/Main.java
+++ carPID/src/Main.java
@@ -3,19 +3,46 @@ import java.awt.*;
 
 public class Main {
 
-    final static int FRAME_H = 500;
-    final static int FRAME_W = 500;
-
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("test");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        CarSimulator myCar = new CarSimulator();
+        Thread myThread = new Thread(myCar);
+        Display display = new Display(myCar);
 
-        JLabel speedLabel = new JLabel("0", SwingConstants.CENTER);
-        frame.add(speedLabel, BorderLayout.CENTER);
+        myThread.start();
+        myCar.setAcceleration(10);
 
+        while(myCar.getSpeed() < 100){
+            display.update();
+        }
+        myCar.stop();
+    }
 
-        frame.setSize(FRAME_W, FRAME_H);
-        frame.setVisible(true);
+    private static class Display {
+
+        private int height = 400;
+        private int width = 400;
+        private CarSimulator car;
+
+        JFrame frame;
+        JLabel speedLabel;
+
+        Display (CarSimulator car) {
+            this.car = car;
+
+            this.frame = new JFrame("myCarPID");
+            this.speedLabel = new JLabel("0", SwingConstants.CENTER);
+
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(speedLabel, BorderLayout.CENTER);
+
+            frame.setSize(width, height);
+            frame.setVisible(true);
+        }
+
+        private void update() {
+            double speed = car.getSpeed();
+            speedLabel.setText(String.format("%.2f Km/h", speed));
+        }
     }
 }
